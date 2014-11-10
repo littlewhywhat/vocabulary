9198369e1c07bc7df56dd71e71e5e37294484656
roman <littlewhywhat@gmail.com>
2014-11-11 00:45:58 +0300

    wordparser: fixed to pass all tests

--- python/wordparser.py
+++ python/wordparser.py
@@ -2,9 +2,8 @@
 def parseWords(string) :
     words = []
     results = [string]
-    divideElemsBy(',', results)
     divideElemsBy(';', results)
-    divideElemsBy(' and ', results)
+    divideElemsByAND_COMA(results)
     length = len(results)
     for result in results :
         result = result.strip()
@@ -18,17 +17,22 @@ def divideElemsBy(divider, elems) :
         string = elems.pop()
         if divider in string :
             results = string.split(divider)
-            check = True
-            if divider != ';' :
-                for result in results :
-                    result = result.strip()
-                    # print result
-                    if ' ' in result:
-                        check = False
-            if check :
-                elems[len(elems) :] = results
-            else :
-                elems.insert(0, string)
+            for result in results :
+                elems.insert(0, result)
         else :
             elems.insert(0, string)
-    # print elems
+            
+def divideElemsByAND_COMA(elems) :
+    length = len(elems)
+    for i in range(0, length) :
+        results = [elems.pop()]
+        check = True
+        divideElemsBy(',', results)
+        divideElemsBy(' and ', results)
+        for result in results :
+            result = result.strip()
+            if ' ' in result :
+                check = False
+        if check :
+            for result in results :
+                    elems.insert(0, result)
